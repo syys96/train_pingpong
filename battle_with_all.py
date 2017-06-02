97a0ebb3a94db8b2e1c63ae377e0dcaf0709f594
syys96 <3359652182@qq.com>
2017-06-02 20:22:14 +0800

    Create battle_with_all.py

---
+++ battle_with_all.py
@@ -0,0 +1,91 @@
+import random
+import os
+import show
+from table import Table, LogEntry, RacketData, BallData, CardData, DIM, TMAX, PL, RS
+import shelve,time,testcode
+
+def race(west_name, west_serve, west_play, west_summarize,
+         east_name, east_serve, east_play, east_summarize):
+    # 生成球桌
+    main_table = Table()
+    main_table.players['West'].bind_play(west_name, west_serve, west_play, west_summarize)
+    main_table.players['East'].bind_play(east_name, east_serve, east_play, east_summarize)
+    log = list()
+
+    # 读取历史数据，文件名为"DS-<name>"
+    for side in ('West', 'East'):
+        d = shelve.open('DS-%s' % (main_table.players[side].name,))
+        try:
+            ds = d['datastore']
+        except KeyError:  # 如果这个文件没有内容，说明球队尚未建立历史数据
+            ds = dict()
+        finally:
+            d.close()
+        main_table.players[side].set_datastore(ds)
+
+    # 发球
+    main_table.serve()
+
+    # 开始打球
+    while not main_table.finished:
+        # 记录日志项
+        log.append(LogEntry(main_table.tick,
+                            RacketData(main_table.players[main_table.side]),
+                            RacketData(main_table.players[main_table.op_side]),
+                            BallData(main_table.ball),
+                            CardData(main_table.card_tick, main_table.cards, main_table.active_card)))
+        # 运行一趟
+        main_table.time_run()
+
+    # 记录最后的回合
+    log.append(LogEntry(main_table.tick,
+                        RacketData(main_table.players[main_table.side]),
+                        RacketData(main_table.players[main_table.op_side]),
+                        BallData(main_table.ball),
+                        CardData(main_table.card_tick, main_table.cards, main_table.active_card)))
+
+    # 终局，让双方进行本局总结
+    main_table.postcare()
+
+    print("%s win! for %s, West:%s(%d）, East:%s(%d),总时间: %d tick" %
+          (main_table.winner, main_table.reason,
+           west_name, main_table.players['West'].life,
+           east_name, main_table.players['East'].life, main_table.tick))
+    global w_win,e_win
+    if main_table.winner == 'West' :
+        w_win += 1
+    else:
+        e_win += 1
+    #time.sleep(5)
+    #show.main(log,main_table.winner,main_table.reason)
+    #testcode.main(log,main_table.winner,main_table.reason)
+
+
+def generate_factors(number) :
+    retFacs = []
+    for i in range(number) :
+        a, b, c,d = random.randint(0, 100), random.randint(0, 100), random.randint(0, 100),random.randint(0,100)
+        a, b, c,d = a / (a + b + c+d), b / (a + b + c+d), c / (a + b + c+d),d/(a+b+c+d)
+        self = [a, b, c,d]
+        a, b, c = random.randint(0, 100), random.randint(0, 100), random.randint(0, 100)
+        a, b, c = a / (a + b + c), b / (a + b + c), c / (a + b + c)
+        ennmy = [a, b, c]
+        retFacs.append((self,ennmy))
+    return retFacs
+
+players = [f[:-3] for f in os.listdir('.') if os.path.isfile(f) and f[-3:] == '.py' and f[:2] == 'T_']
+for i in range(len(players)) :
+    print(i,' ',players[i])
+print('选择测试者:(请输入序号,从0开始)')
+west_index = int(input())
+west_name = players[west_index]
+exec('import %s as WP' % (west_name,))
+w_win, e_win = 0,0
+for i in range(len(players)):
+    east_name = players[i]
+    if i == west_index :
+        continue
+    exec('import %s as EP' % (east_name,))
+    race(west_name, WP.serve, WP.play, WP.summarize, east_name, EP.serve, EP.play, EP.summarize)
+print(w_win,e_win)
+
